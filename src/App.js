0ebbb493ea34e33f0431c51d082375f00a0b2039
xoxlushka1337 <xoxlushka@gmail.com>
2023-07-05 19:31:59 +0300

    feat: add counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import './index.scss';
+import React from "react";
+import "./index.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
-  );
+	const [count, setCount] = React.useState(0);
+
+	const onClickPlus = () => {
+		setCount(count + 1);
+	};
+
+	const onClickMinus = () => {
+		if (count !== 0) {
+			setCount(count - 1);
+		}
+	};
+
+	return (
+		<div className="App">
+			<div>
+				<h2>Счетчик:</h2>
+				<h1>{count}</h1>
+				<button onClick={onClickMinus} className="minus">
+					- Минус
+				</button>
+				<button onClick={onClickPlus} className="plus">
+					Плюс +
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
